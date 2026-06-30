39064a3d60a5bab329fbca4e7b9677308dbd4318
Pranay3602 <pranayasnani@gmail.com>
2026-06-30 18:18:28 +0400

    initial commit

---
+++ trigonometric/trigonometric.py
@@ -0,0 +1,6 @@
+import math
+angle=float(input("enter angles in degrees: "))
+radian=math.radians(angle)
+print("Sin =", math.sin(radian))
+print("con =",math.cos(radian))
+print("tan =", math.tan(radian))
