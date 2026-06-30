a8efffd415911bd275f302f4e1d72920b6c4291e
Pranay3602 <pranayasnani@gmail.com>
2026-06-30 18:28:57 +0400

    initial commit

---
+++ months/month.py
@@ -0,0 +1,6 @@
+list=("january, feburary, march, april, june, july, august, september, october, november, december")
+answer=(input("would you like to know the months of the year?y/n"))
+if answer=="y":
+    print(list)
+else:
+    print("ok you may move on with your life")
