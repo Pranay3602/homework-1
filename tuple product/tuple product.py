8c6c0d4444bf609b7fbda8dcc2fa9dcbe3932581
Pranay3602 <pranayasnani@gmail.com>
2026-07-08 11:18:58 +0400

    initial commit

---
+++ tuple product/tuple product.py
@@ -0,0 +1,9 @@
+outcome=input("what did the first number reach")
+outcome2=input("what did the second number reach")
+outcome3=input("what did the third number reach")
+tuple=(outcome, outcome2, outcome3)
+number=float(outcome)
+number2=float(outcome2)
+number3=float(outcome3)
+product=number*number2*number3
+print("the product is", product)
